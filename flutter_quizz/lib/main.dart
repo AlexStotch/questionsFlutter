ea3c26ecea840e74345b3f746f1f032e296dcf6d
Alex <ergo.picault@gmail.com>
2020-05-03 14:26:36 +0200

    widget refacto

--- flutter_quizz/lib/main.dart
+++ flutter_quizz/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quizz/result.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,7 +13,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final questions = const [
+  final _questions = const [
     {
       'questionText': 'Quel est ta couleur pref ?',
       'answers': ['Bleu', 'Jaune', 'Rose', 'Vert'],
@@ -34,27 +34,24 @@ class _MyAppState extends State<MyApp> {
       // call build()
       _questionIndex = _questionIndex + 1;
     });
-    if (_questionIndex < questions.length) {}
+    if (_questionIndex < _questions.length) {}
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('First App'),
-          ),
-          body: _questionIndex < questions.length
-              ? Column(children: [
-                  Question(questions[_questionIndex]['questionText']),
-                  ...(questions[_questionIndex]['answers'] as List<String>)
-                      .map((answer) {
-                    return Answer(_answerQuestion, answer);
-                  }).toList()
-                ])
-              : Center(
-                  child: Text('Bravo le veau'),
-                )),
+        appBar: AppBar(
+          title: Text('First App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(),
+      ),
     );
   }
 }
