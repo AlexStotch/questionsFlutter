f1537ff10aaf760cec887cd17b263e91f581d9d7
Alex <ergo.picault@gmail.com>
2020-05-03 15:11:05 +0200

    finalelize app

--- flutter_quizz/lib/main.dart
+++ flutter_quizz/lib/main.dart
@@ -15,21 +15,45 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
-      'questionText': 'Quel est ta couleur pref ?',
-      'answers': ['Bleu', 'Jaune', 'Rose', 'Vert'],
+      'questionText': 'Quel est ma couleur pref ?',
+      'answers': [
+        {'text': 'Vert', 'score': 4},
+        {'text': 'Jaune', 'score': 1},
+        {'text': 'Rose', 'score': 2},
+        {'text': 'Bleu', 'score': 5}
+      ],
     },
     {
-      'questionText': 'Quel est ton animal pref ?',
-      'answers': ['Chat', 'Chien', 'Lapin', 'Elephant'],
+      'questionText': 'Quel est mon animal pref ?',
+      'answers': [
+        {'text': 'Chat', 'score': 2},
+        {'text': 'Lapin', 'score': 1},
+        {'text': 'Chien', 'score': 5},
+        {'text': 'Elephant', 'score': 4}
+      ],
     },
     {
-      'questionText': 'Quel est ton pref ?',
-      'answers': ['Annie Brie', 'Claude', 'Harrry', 'Jorda,'],
+      'questionText': 'Quel est mon pref ?',
+      'answers': [
+        {'text': 'Annie Brie', 'score': 5},
+        {'text': 'Alexandra KohLanta', 'score': 1},
+        {'text': 'Claude KohLanta', 'score': 2},
+        {'text': 'katy Perry', 'score': 4}
+      ],
     },
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
     setState(() {
       // call build()
       _questionIndex = _questionIndex + 1;
@@ -50,7 +74,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- flutter_quizz/lib/result.dart
+++ flutter_quizz/lib/result.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'Trop fort tu me connais bof bien';
+    } else if (resultScore <= 10) {
+      resultText = 'Trop fort tu me connais moyen bien';
+    } else if (resultScore <= 15) {
+      resultText = 'Trop fort tu me connais trop bien';
+    } else {
+      resultText = 'Trop fort tu me connais pas bien';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Bravo le veau'),
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text('Restart Quiz'),
+            textColor: Colors.blue,
+            onPressed: resetHandler,
+          )
+        ],
+      ),
     );
   }
 }
